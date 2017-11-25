bf0c437f63ea1720ab3224dec20ce03b22fed450
Lucas Peng <lucaspeng@Lucass-MacBook-Pro.local>
2017-11-24 18:35:15 -0800

    rename some variables.

--- hqtrivia.py
+++ hqtrivia.py
@@ -14,12 +14,12 @@ import urllib
 class HQTrivia:
 #    cam_size = (1280, 720)
     cam_size = (1920, 1080)
-    capture_size = (int(700),int(280))
+    question_size = (int(700),int(280))
     answer_size = (int(650),int(350))
-    capture_rect = ((int(cam_size[0]/2-capture_size[0]/2+70),
-                     int(cam_size[1]/2-capture_size[1]/2)),
-                    (int(cam_size[0]/2+capture_size[0]/2+70),
-                     int(cam_size[1]/2+capture_size[1]/2)))
+    question_rect = ((int(cam_size[0]/2-question_size[0]/2+70),
+                     int(cam_size[1]/2-question_size[1]/2)),
+                    (int(cam_size[0]/2+question_size[0]/2+70),
+                     int(cam_size[1]/2+question_size[1]/2)))
     answer_rect  = ((int(cam_size[0]/2-answer_size[0]/2+70),
                      int(cam_size[1]/2-answer_size[1]/2 + 350)),
                     (int(cam_size[0]/2+answer_size[0]/2+70),
@@ -108,7 +108,7 @@ class HQTrivia:
                 gray_flip = cv2.flip(gray,1)
             else:
                 gray_flip = gray
-            cv2.rectangle(gray_flip, self.capture_rect[0], self.capture_rect[1], (0,0,0))
+            cv2.rectangle(gray_flip, self.question_rect[0], self.question_rect[1], (0,0,0))
             cv2.rectangle(gray_flip, self.answer_rect[0], self.answer_rect[1], (0,0,0))
             # Display the resulting frame
             cv2.imshow('frame',gray_flip)
@@ -116,24 +116,24 @@ class HQTrivia:
             key = cv2.waitKey(1) & 0xFF
             if key == ord('c'):
                 print('capture!')
-                cap_p1 = self.capture_rect[0]
-                cap_p2 = self.capture_rect[1]
-                cap_img = gray[cap_p1[1]:cap_p2[1], cap_p1[0]:cap_p2[0]]
+                q_p1 = self.question_rect[0]
+                q_p2 = self.question_rect[1]
+                q_img = gray[q_p1[1]:q_p2[1], q_p1[0]:q_p2[0]]
                 ans_p1 = self.answer_rect[0]
                 ans_p2 = self.answer_rect[1]
                 ans_img = gray[ans_p1[1]:ans_p2[1], ans_p1[0]:ans_p2[0]]
-                #cap_img = cv2.medianBlur(cap_img,5)
-                #cap_img = cv2.GaussianBlur(cap_img,(3,3),0)
-                ret,cap_img = cv2.threshold(cap_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+                #q_img = cv2.medianBlur(q_img,5)
+                #q_img = cv2.GaussianBlur(q_img,(3,3),0)
+                ret,q_img = cv2.threshold(q_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
                 #ans_img = cv2.GaussianBlur(ans_img,(5,5),0)
                 #ret,ans_img = cv2.threshold(ans_img,200,255,cv2.THRESH_BINARY)
                 ret,ans_img = cv2.threshold(ans_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
                 #ans_img = cv2.adaptiveThreshold(ans_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-                #cap_img = cv2.adaptiveThreshold(cap_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-                cv2.imwrite('test.png',cap_img)
+                #q_img = cv2.adaptiveThreshold(q_img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
+                cv2.imwrite('test.png',q_img)
                 cv2.imwrite('test_ans.png',ans_img)
                 txt = self.tool.image_to_string(
-                    Image.fromarray(cap_img),
+                    Image.fromarray(q_img),
                     lang=self.lang,
                     builder=pyocr.builders.TextBuilder()
                 )
